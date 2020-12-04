87a7a2ae3570a5889039662ff55d6cd940d32600
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-04 23:12:29 +0100

    Clickable interface

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,26 +19,38 @@ public class PlayerMovement : MonoBehaviour
     {
         MovePlayer();
         RotateCamera();
+
+        if (Input.GetButton("Fire1")) {
+            HandleClick();
+		}
     }
 
     private void MovePlayer()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-        
-        float horizontal = speed * Input.GetAxis("Horizontal");
-        float vertical = speed * Input.GetAxis("Vertical");
-        Vector3 moveDir = vertical * forward + horizontal * right;
+        Vector3 moveDir = horizontal * right + vertical * forward;
 
-        cc.Move(moveDir * Time.deltaTime);
+        cc.Move(speed * Time.deltaTime * moveDir);
     }
 
     private void RotateCamera()
 	{
         float horizontal = sensitivity * Input.GetAxis("Mouse X");
-        transform.rotation *= Quaternion.Euler(0f, horizontal, 0f);
-
         float vertical = -sensitivity * Input.GetAxis("Mouse Y");
+
+        transform.rotation *= Quaternion.Euler(0f, horizontal, 0f);
         Camera.main.transform.rotation *= Quaternion.Euler(vertical, 0f, 0f);
     }
+
+    private void HandleClick()
+	{
+        Transform cam = Camera.main.transform;
+        if (Physics.Raycast(cam.position, cam.forward, out var hit, 10f)) {
+            hit.collider.GetComponent<IClickable>()?.HandleClick();
+        }
+	}
 }
---
+++ Assets/Scripts/WorkerTable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class WorkerTable : MonoBehaviour, IClickable
+{
+	public void HandleClick()
+	{
+		Debug.Log("clicl");
+	}
+}
