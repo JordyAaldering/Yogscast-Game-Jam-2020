ea2950116f6dbb510087b7f93988b9c9ae3a2c8f
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-05 00:16:38 +0100

    Player table

---
+++ Assets/Scripts/Table/PlayerTable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerTable : MonoBehaviour, IClickable
+{
+	[SerializeField] private int efficiency;
+	[SerializeField] private float cooldown;
+	private float cooldownLeft;
+
+	private void Update()
+	{
+		if (cooldownLeft > 0f) {
+			cooldownLeft -= Time.deltaTime;
+		}
+	}
+
+	public void HandleClick()
+	{
+		if (cooldownLeft <= 0f) {
+			cooldownLeft = cooldown;
+			PlayerStatsManager.Instance.PresentsTotal += efficiency;
+		}
+
+	}
+}
