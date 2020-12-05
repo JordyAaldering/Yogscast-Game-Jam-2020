9176e5dd86b8feb24cad0b1087c26f489311f445
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-05 14:22:29 +0100

    Progress panel

---
+++ Assets/Scripts/UI/ProgressPanel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class ProgressPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI progressText;
+
+	private Dictionary<string, Tuple<int, int>> progress;
+
+	private void Start()
+	{
+		PopulateProgress();
+		UpdateText();
+	}
+
+	private void PopulateProgress()
+	{
+		progress = new Dictionary<string, Tuple<int, int>>();
+		foreach (Table t in FindObjectsOfType<Table>()) {
+			if (progress.TryGetValue(t.tableName, out var val)) {
+				progress[t.tableName] = new Tuple<int, int>(
+					val.Item1 + (t.IsBought ? 1 : 0),
+					val.Item2 + 1
+				);
+			} else {
+				progress.Add(t.tableName, new Tuple<int, int>(t.IsBought ? 1 : 0, 1));
+			}
+		}
+	}
+
+	public void UpdateText()
+	{
+		progressText.text = "";
+		foreach (var pair in progress) {
+			progressText.text += $"{pair.Key}: {pair.Value.Item1}/{pair.Value.Item2}\n";
+		}
+		progressText.text.Remove(progressText.text.Length - 1);
+	}
+}
