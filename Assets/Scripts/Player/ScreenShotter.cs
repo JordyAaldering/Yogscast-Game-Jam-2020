a328936415edd2ac18d835e438b099ce2b481d8b
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-06 14:18:24 +0100

    Screenshots

---
+++ Assets/Scripts/Player/ScreenShotter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class ScreenShotter : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F12)) {
+			ScreenCapture.CaptureScreenshot($"Screenshots/{DateTime.Now}");
+		}
+	}
+}
