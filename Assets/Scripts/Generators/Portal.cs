d9192d250ca87075a7e5949bc387fcba8f8ff063
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-06 13:24:40 +0100

    Portal

---
+++ Assets/Scripts/Generators/Portal.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Portal : Generator
+{
+	[SerializeField] private GameObject enabledOnBuy;
+	[SerializeField] private float clickCooldown;
+	private float cooldown;
+
+	private void Awake()
+	{
+		Cost = buyCost;
+		enabledOnBuy.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (cooldown > 0f) {
+			cooldown -= Time.deltaTime;
+		}
+	}
+
+	public override void HandleClick()
+	{
+		if (cooldown <= 0f) {
+			PlayerStatsManager.Instance.PresentsTotal += Efficiency;
+			PlayerStatsManager.Instance.Happiness += Efficiency * PlayerStatsManager.Instance.HappinessModifier;
+			cooldown = clickCooldown;
+		}
+	}
+	
+	public override void HandleInteract()
+	{
+		if (PlayerStatsManager.Instance.PresentsTotal < Cost) {
+			return;
+		}
+
+		PlayerStatsManager.Instance.PresentsTotal -= Cost;
+		Cost = (int)(Cost * upgradeCostMultiplier);
+
+		if (!IsBought) {
+			PlayerStatsManager.Instance.Efficiency += initialEfficiency;
+			FindObjectOfType<ProgressPanel>().IncreaseCounter(tableName);
+			Efficiency = initialEfficiency;
+
+			enabledOnBuy.SetActive(true);
+			IsBought = true;
+		} else {
+			PlayerStatsManager.Instance.Efficiency += upgradeEfficiencyIncrease;
+			Efficiency += upgradeEfficiencyIncrease;
+		}
+	}
+}
